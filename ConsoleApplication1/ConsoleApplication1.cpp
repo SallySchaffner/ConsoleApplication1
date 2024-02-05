99b169ef12b0d2a68b9e91930e9f6c97ae47fa0a
Sally Schaffner <SchaffnerS@vpcc.edu>
2024-02-05 15:12:05 -0500

    Added output file.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,20 +3,29 @@
 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
 	ifstream inFile;
 	inFile.open("SalaryData.txt");
+	ofstream outFile;
+	outFile.open("Salary.out");
 	
 
 	string lastName, firstName;
 	double salary, increase;
 
-	inFile >> lastName >> firstName >> salary >> increase;
-	cout << lastName << " " << firstName << " " << salary << " " << increase << endl;
+	outFile << fixed << setprecision(2) << showpoint;
 
-	
+	inFile >> lastName >> firstName >> salary >> increase;
+	outFile << lastName << " " << firstName << " " << salary << " " << increase << endl;
+
+	inFile >> lastName >> firstName >> salary >> increase;
+	outFile << lastName << " " << firstName << " " << salary << " " << increase << endl;
+
+	outFile.close();
+	return 0;
 }
 
